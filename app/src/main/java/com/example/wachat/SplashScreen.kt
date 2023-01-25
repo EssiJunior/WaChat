ada2c3d5e17dc7a681c93b4cd66a530202d1144a
EssiJunior <nessipjunior@gmail.com>
2023-01-25 23:17:43 +0100

    DONE: Splash screen ----- TODO: Implement navgraph

---
+++ app/src/main/java/com/example/wachat/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.example.wachat
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            // Delay and Start Activity
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } , 2000) // here we're delaying to startActivity after 2seconds
+    }
+}
